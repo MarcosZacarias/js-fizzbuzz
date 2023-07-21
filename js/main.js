bd9c8fe3e7350427c23a49a3f8586dcfa195cd18
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-07-21 17:05:50 +0200

    add boxes and number inside

--- js/main.js
+++ js/main.js
@@ -1,11 +1,30 @@
+const row = document.getElementById("row");
+
 for (let i = 1; i <= 100; i++) {
+  const box = document.createElement("div");
+  box.style.width = "calc(100% / 7)";
+  const numberView = document.createElement("p");
+  numberView.classList.add("bg-primary", "d-flex", "p-2");
+  numberView.style.aspectRatio = "1";
+  numberView.style.justifyContent = "center";
+  numberView.style.alignItems = "center";
+  numberView.style.backgroundColor = "bg-primary";
+
+  let number = i;
   if (i % 3 == 0 && i % 5 == 0) {
-    console.log("FizzBuzz");
+    number = "FizzBuzz";
+    numberView.classList.add("bg-danger");
   } else if (i % 3 == 0) {
-    console.log("Fizz");
+    number = "Fizz";
+    numberView.classList.add("bg-success");
   } else if (i % 5 == 0) {
-    console.log("Buzz");
-  } else {
-    console.log(i);
+    number = "Buzz";
+    numberView.classList.add("bg-warning");
   }
+  console.log(number);
+
+  numberView.innerHTML = number;
+
+  box.appendChild(numberView);
+  row.appendChild(box);
 }
