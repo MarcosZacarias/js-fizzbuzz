6982d0c0fadef2fd4a8af745d46f1fbaa7a03287
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-07-21 17:45:47 +0200

    add: class "box" and "number-View"

--- js/main.js
+++ js/main.js
@@ -2,18 +2,14 @@ const row = document.getElementById("row");
 
 for (let i = 1; i <= 100; i++) {
   const box = document.createElement("div");
-  box.style.width = "calc(100% / 7)";
+  box.classList.add("box");
   const numberView = document.createElement("p");
-  numberView.classList.add("bg-primary", "d-flex", "p-2");
-  numberView.style.aspectRatio = "1";
-  numberView.style.justifyContent = "center";
-  numberView.style.alignItems = "center";
-  numberView.style.backgroundColor = "bg-primary";
+  numberView.classList.add("number-View");
 
   let number = i;
   if (i % 3 == 0 && i % 5 == 0) {
     number = "FizzBuzz";
-    numberView.classList.add("bg-danger");
+    numberView.classList.add("bg-warning");
   } else if (i % 3 == 0) {
     number = "Fizz";
     numberView.classList.add("bg-success");
